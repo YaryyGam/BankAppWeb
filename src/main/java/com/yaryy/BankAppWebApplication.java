202a946e2ad9766f741ac03453c65db2d714f1f8
YaryyGam <gamerplay0606@gmail.com>
2024-10-06 21:59:21 +0300

    Initial commit

---
+++ src/main/java/com/yaryy/BankAppWebApplication.java
@@ -0,0 +1,13 @@
+package com.yaryy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankAppWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankAppWebApplication.class, args);
+	}
+
+}
